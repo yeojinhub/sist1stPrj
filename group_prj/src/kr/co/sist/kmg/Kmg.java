00ee8e0849ccd0815007bb5e9c3cdf7ca2d746a2
user <user@DESKTOP-3HCOD5U>
2025-03-31 12:42:10 +0900

    민경 - 3번째

--- group_prj/src/kr/co/sist/kmg/Kmg.java
+++ group_prj/src/kr/co/sist/kmg/Kmg.java
@@ -5,7 +5,7 @@ public class Kmg {
 	public static void main(String[] args) {
 		System.out.println("자신이 구현해야할 코드 - 개발하는 곳");
 		
-		System.out.println("나는 KMG야!!!");
+		System.out.println("나는 KMG야!!!3");
 
 	}
 
