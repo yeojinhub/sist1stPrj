9bd3f8cc04252d8d75bb05667eb40756d80002b1
user <user@DESKTOP-3HCOD5U>
2025-03-31 12:39:28 +0900

    민경-내가했어

---
+++ group_prj/src/kr/co/sist/kmg/Kmg.java
@@ -0,0 +1,12 @@
+package kr.co.sist.kmg;
+
+public class Kmg {
+
+	public static void main(String[] args) {
+		System.out.println("자신이 구현해야할 코드 - 개발하는 곳");
+		
+		System.out.println("나는 KMG야!");
+
+	}
+
+}
